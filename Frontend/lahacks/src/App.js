0115589e2e7b985a2609ff345ab38084b6b0ceeb
Manan Shah <manan228@yahoo.in>
2024-04-20 23:08:14 -0700

    Initial commit

---
+++ Frontend/lahacks/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  const onClickHandler = async () => {
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = axios.post("http://localhost:5000/upload", formData);
+
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="App">
+      <input type="file" name="image" id="fileInput" />
+      <button onClick={onClickHandler}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
