edd4b9e2bb75bfb63addfda7fc9f1e4da71e8b16
Dhruvil Mehta <68022411+dhruvilmehta@users.noreply.github.com>
2024-04-21 00:02:12 -0700

    Added backend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,25 @@ from IPython.display import Markdown
 import textwrap
 from PIL import Image
 import io
+import torch
+
 genai.configure(api_key="AIzaSyDUIy8e6M5KTS348R1rQFWabw1KtYExXx4")
+loaded_model = torch.load('model.pth', map_location=torch.device('cpu'))
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# USE_CPU = False
+# device = "xpu:0" if torch.xpu.is_available() else "cpu"
+# if USE_CPU:
+#     device = "cpu"
+# print(f"using device: {device}")
+
+model_id = "google/gemma-2b"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+
 # instance of flask application
 app = Flask(__name__)
  
@@ -22,6 +40,7 @@ def to_markdown(text):
 def uploadPhoto():
     # img = PIL.Image.open('image.jpg')
     if request.method == 'POST':
+        print("Request received")
         # Check if the POST request has the file part
         if 'file' not in request.files:
             return 'No file part'
@@ -34,11 +53,21 @@ def uploadPhoto():
         model = genai.GenerativeModel('gemini-1.5-pro-latest')
         response = model.generate_content(["Give detailed information about the image.", img], stream=True)
         response.resolve()
-        formatted_response = to_markdown(response.text)
-        print(formatted_response.data)
+        response = to_markdown(response.text).data
+        print("Got response from gemini")
+        print(response)
+        print("Running custom model")
+        # for text in test_inputs:
+        inputs = tokenizer(response, return_tensors="pt")
+        print("feeding inputs")
+        outputs = loaded_model.generate(**inputs, max_new_tokens=100, 
+                                do_sample=False, top_k=50,temperature=0.1, 
+                                eos_token_id=tokenizer.eos_token_id)
+        print(tokenizer.decode(outputs[0], skip_special_tokens=True))
         return {
-            "data": formatted_response.data
+            "data": response
         }
+    return "Not allowed"
  
 if __name__ == '__main__':  
    app.run()
